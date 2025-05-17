081ed9175dc0c6c422a0652eb496e5c7b67bd347
shintopc <50198933+shintopc@users.noreply.github.com>
2025-05-16 18:29:23 -0700

    Update firebase-config.js

--- public/scripts/firebase-config.js
+++ public/scripts/firebase-config.js
@@ -1,1 +1,23 @@
 
+// Initialize Firebase using your config
+const firebaseConfig = {
+  apiKey: "AIzaSyCRa1qLfVfRvxl2KOvNpocNj7Z2xnuDt9Q",
+  authDomain: "bustimingsapp.firebaseapp.com",
+  projectId: "bustimingsapp",
+  storageBucket: "bustimingsapp.firebasestorage.app",
+  messagingSenderId: "201532782611",
+  appId: "1:201532782611:web:a57dc47f2613c4afdfa7e6"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+
+// Enable offline persistence
+db.enablePersistence()
+  .catch((err) => {
+    console.log("Firebase persistence error:", err);
+  });
+
+// Export database methods
+export { db, firebase };
