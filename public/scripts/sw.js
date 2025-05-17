2da8568f34b39d5ff495f30c830e75028f675332
shintopc <50198933+shintopc@users.noreply.github.com>
2025-05-16 18:33:34 -0700

    Create sw.js

---
+++ public/scripts/sw.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'bus-timing-v2';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/admin.html',
+  '/styles/main.css',
+  '/scripts/app.js',
+  '/scripts/admin.js',
+  '/scripts/firebase-config.js',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
