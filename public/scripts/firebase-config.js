7203a8968cea3eed8f44e4eb9a9f433091ef68d0
shintopc <50198933+shintopc@users.noreply.github.com>
2025-05-16 19:15:57 -0700

    Update firebase-config.js

--- public/scripts/firebase-config.js
+++ public/scripts/firebase-config.js
@@ -1,12 +1,12 @@
 
 // Initialize Firebase using your config
 const firebaseConfig = {
-  apiKey: "AIzaSyCRa1qLfVfRvxl2KOvNpocNj7Z2xnuDt9Q",
-  authDomain: "bustimingsapp.firebaseapp.com",
-  projectId: "bustimingsapp",
-  storageBucket: "bustimingsapp.firebasestorage.app",
-  messagingSenderId: "201532782611",
-  appId: "1:201532782611:web:a57dc47f2613c4afdfa7e6"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
